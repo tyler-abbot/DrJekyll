14ba0ec77c09c82fec68d41b8a4c51b25c1872e2
Tyler Abbot <tyler.abbot@sciencespo.fr>
2016-05-15 19:18:24 +0200

    First Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Dr. Jekyll: A scholarly website template.
+===================
+
+This Jekyll template is meant for young (or established) scholars who would like to set up a website quickly and have it remain stable in the future.
+
+The template includes a built in bibliography using bibtex format that you can use to update your incredibly long publication list (knock on wood).
---
+++ _plugins/ext.rb
@@ -0,0 +1,19 @@
+require 'jekyll/scholar'
+require 'uri'
+
+module HTMLFilter
+  class HTML < BibTeX::Filter
+    def apply(value)
+      links_list = value.slice(URI.regexp(['http','https','ftp'])).to_s.split(";")
+      if links_list.size > 0
+        value = ""
+        for index in 0 ... links_list.size
+          link_text = links_list[index].slice!(URI.regexp(['LINK'])).to_s.slice(5..-1)
+          links_list[index] = "[<a href=\"#{links_list[index]}\">#{link_text}</a>]"
+          value = value + links_list[index]
+        end
+      end
+      return value
+    end
+  end
+end
